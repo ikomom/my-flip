fd76572ea85cf98f79295333a643161d646e04eb
yzm <315793076@qq.com>
2022-07-13 09:38:07 +0800

    add nprogress

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
+import nProgress from 'nprogress'
+import 'nprogress/nprogress.css' // progress bar style
+
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
@@ -12,5 +15,14 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
+nProgress.configure({ showSpinner: false, trickleSpeed: 200 })
+router.beforeEach(() => {
+  nProgress.start()
+})
+
+router.afterEach(() => {
+  nProgress.done()
+})
+
 app.use(router)
 app.mount('#app')
