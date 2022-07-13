e9c317a98ac498e15fe8e25d68f0d066a3c7ec98
yzm <315793076@qq.com>
2022-07-13 09:14:32 +0800

    foo

--- src/pages/Foo.vue
+++ src/pages/Foo.vue
@@ -25,10 +25,10 @@ function reset() {
       </button>
     </div>
     <div m10 flex="~ col sm:row">
-      <float-proxy flex-shrink-0 rounded="1/2" :style="{ width: `${size}px`, height: `${size}px` }" />
       <p flex-auto>
         Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
       </p>
+      <float-proxy flex-shrink-0 rounded="1/2" :style="{ width: `${size}px`, height: `${size}px` }" />
     </div>
   </div>
 </template>
