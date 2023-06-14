7207493aa4a0fd662cfba1148c1fb897805a1d4f
ikonon <315793076@qq.com>
2023-06-14 23:16:59 +0800

    feat: parallel Test

--- src/pages/comp/modules/pool/upload/parallel/test.ts
+++ src/pages/comp/modules/pool/upload/parallel/test.ts
@@ -1,14 +1,40 @@
 // import WorkerUri from '../worker/fileMd5.worker?worker'
-import { cloneDeep } from 'lodash-es'
 import workerpool from 'workerpool'
 
+enum TaskStatus {
+  READY,
+  PENDING,
+  SUCCESS,
+  ERROR,
+}
+
+// 1. 许多耗时的异步任务
+// 2. 把这些任务放到列表中
+// 3. 限制最大运行的异步任务数，减小浏览器内存开销
+// 4. 任务运行成功后或者新加任务后，检查队列，检查是否有需要运行的任务
 class Task {
   private _ready = false
+  private _status: TaskStatus = TaskStatus.READY
+
   constructor(public name: string) {
+  }
+
+  get status() {
+    return this._status
+  }
+
+  setStatus(status: TaskStatus) {
+    this._status = status
   }
 
   start() {
     this._ready = false
+    return new Promise((resolve) => {
+      const random = 1000 * Math.random()
+      setTimeout(() => {
+        resolve(random)
+      }, random)
+    })
   }
 
   terminal() {
@@ -23,8 +49,8 @@ function proxyLog(obj: any) {
       return Reflect.get(target, property)
     },
     set(target, property, value, receiver) {
-      if (property !== 'length')
-        console.log('[proxyLog set]', cloneDeep({ property, value }))
+      // if (property !== 'length')
+      //   console.log('[proxyLog set]', cloneDeep({ property, value }))
 
       Reflect.set(target, property, value, receiver)
       return true
@@ -34,29 +60,45 @@ function proxyLog(obj: any) {
 function toPlain(obj: any) {
   return JSON.parse(JSON.stringify(obj))
 }
-
+interface QueueItem { task: Task; resolve: any; reject: any; args: any[] }
 function createPool({ max = 4 } = {}) {
-  const queue: any[] = proxyLog([])
-  const runningQueue: any[] = []
+  const queue: QueueItem[] = proxyLog([])
+  let _max = max
 
-  // const workMap = new WeakMap<any, any>()
-  //
+  function taskFilter(s: QueueItem) {
+    return s.task.status === TaskStatus.READY || s.task.status === TaskStatus.ERROR
+  }
 
-  // workMap.set('1', () => {})
-  // queue.push(worker1)
-  // queue.push(worker2)
+  function log(...args: any[]) {
+    // console.log(...args)
+  }
 
   function _processNext() {
-    while (runningQueue.length < max) {
-      const p = queue.pop()
-      if (p)
-        runningQueue.push(p)
-
-      else
-        break
+    const readyCount = queue.filter(taskFilter).length
+    const successCount = queue.filter(s => s.task.status === TaskStatus.SUCCESS).length
+    log('_processNext', { _max, readyCount, successCount })
+    // eslint-disable-next-line no-unmodified-loop-condition
+    while (readyCount > 0 && _max > 0) {
+      const item = queue.find(taskFilter)
+      log(item, { _max, readyCount })
+      if (!item)
+        return
+      // 释放通道
+      _max--
+      item.task.setStatus(TaskStatus.PENDING)
+      item.task.start().then((res) => {
+        item.task.setStatus(TaskStatus.SUCCESS)
+        item.resolve(res)
+      }).catch((e) => {
+        item.task.setStatus(TaskStatus.ERROR)
+        item.reject(e)
+      }).finally(() => {
+        log('next', toPlain(queue))
+        // 增加通道
+        _max++
+        _processNext()
+      })
     }
-    // TODO: 运行队列，并等待返回
-    console.log({ runningQueue, queue: toPlain(queue) })
   }
 
   console.log({ queue })
@@ -76,7 +118,7 @@ function createPool({ max = 4 } = {}) {
 }
 
 export function runTest() {
-  const { exec } = createPool()
+  const { exec } = createPool({ max: 2 })
   for (let i = 0; i < 6; i++) {
     const task = new Task(`task_${i}`)
     exec(task, [i]).then((res) => {
@@ -91,17 +133,13 @@ const pool = workerpool.pool({
 })
 workerpool.pool({})
 export function runWorkerPool() {
-  async function add(a: number, b: number) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(a + b)
-      }, Math.random() * 5000)
-    })
+  function add(a: number, b: number) {
+    return a + b
   }
 
   pool
     .exec(add, [3, 4])
-    .then((result: number) => {
+    .then((result) => {
       console.log('result', result, workerpool) // outputs 7
     })
     .catch((err: Error) => {
