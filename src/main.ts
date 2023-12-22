9189b9b7888526dac2d06ec4c8ca79ce4423fd4a
orion <orion.ye@aiya.nyc>
2023-12-21 22:06:45 -0500

    feat: fundebug-vue test

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import { installPinia } from '~/composables/pinia'
 import { installNotification } from '~/composables/notification'
 import { router } from '~/router'
+import * as fundebug from "fundebug-javascript";
+// @ts-ignore
+import FundebugVue from "fundebug-vue";
 
 import '@unocss/reset/tailwind.css'
 import './assets/styles/main.css'
@@ -14,9 +17,13 @@ import 'uno.css'
 //     console.log('worker', worker.start())
 //   })
 // }
+fundebug.init({
+  apikey: "51024a11062c24a528ae666bd3c1006e84e6056682a378b16026a4f361493496"
+})
 
 const app = createApp(App)
 installPinia(app)
 installNotification(app)
+app.use(new FundebugVue(fundebug))
 app.use(router)
 app.mount('#app')
