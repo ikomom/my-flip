5e72f3dba72e4511c654cc3c27d7f10a8436faeb
ikomom <yezm@zyaud.com>
2024-07-30 21:09:28 +0800

    feat: cesium demo

---
+++ src/pages/cesium/ShapeFile.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import * as shapefile from "shapefile"
+
+const emits = defineEmits(['load'])
+
+function loadShapeFile() {
+  let geojsonArr = []
+  shapefile.open("/map/福州市.shp", '/map/福州市.dbf', {
+    encoding: 'utf-8'
+  })
+    .then((source) => {
+      source.read().then(function log(result) {
+        if (result.done) {
+          //解析完成
+          emits('load', geojsonArr)
+          return
+        }
+
+        const json = result.value
+        geojsonArr.push(json)
+        return source.read().then(log)
+      })
+    })
+    .catch((error) => {
+      console.error(error.stack)
+    })
+}
+
+</script>
+
+<template>
+  <n-button @click="loadShapeFile" btn>加载shapeFile</n-button>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/pages/cesium/index.vue
+++ src/pages/cesium/index.vue
@@ -3,9 +3,10 @@ import {
   Cartesian2,
   Cartesian3,
   Math as CesiumMath,
-  Ion, SceneTransforms, Terrain, Viewer,
+  Ion, SceneTransforms, Terrain, Viewer, GeoJsonDataSource,
 } from 'cesium'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
+import ShapeFile from "~/pages/cesium/ShapeFile.vue";
 
 // @ts-expect-error
 window.CESIUM_BASE_URL = '/cesiumStatic'
@@ -14,10 +15,11 @@ window.CESIUM_BASE_URL = '/cesiumStatic'
 // Replace `your_access_token` with your Cesium ion access token.
 
 Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiY2Y3NjI1Yy0zOTIzLTRjZDktOTM1Yi05MDRlOWQwY2JiNGMiLCJpZCI6MzUzMTEsImlhdCI6MTYwMTc5Mzg5N30.NYw_Ok48KI4wlQEo3-PPk2wpoLanGGLBArLsZERh_Qk'
+let viewer: Viewer
 
 onMounted(async () => {
   // 使用 'cesiumContainer' ID 初始化 HTML 元素中的 Cesium Viewer。
-  const viewer = new Viewer('cesiumContainer', {
+  viewer = new Viewer('cesiumContainer', {
     terrain: Terrain.fromWorldTerrain(),
   })
 
@@ -46,7 +48,7 @@ onMounted(async () => {
       500,
     )
     viewer.scene.postRender.addEventListener((e) => {
-      console.log('postRender')
+      // console.log('postRender')
       const canvasHeight = viewer.scene.canvas.height
       const windowPosition = new Cartesian2()
       SceneTransforms.wgs84ToWindowCoordinates(
@@ -59,7 +61,7 @@ onMounted(async () => {
       div.style.left = `${windowPosition.x - elWidth / 2}px`
     })
   }
-  addDynamicLabel({ position: [121, 24] })
+  // addDynamicLabel({ position: [121, 24] })
   // const wmtsImageryProvider = new WebMapTileServiceImageryProvider({
   //   // url: 'http://localhost:8080/iserver/services/agscachev-Layers/wmts',
   //   url: 'https://api.mapbox.com/v4/mapbox.satellite/11/1719/875.webp?sku=101iWfSXNEtoQ&access_token=pk.eyJ1IjoidHlwZWJyb29rIiwiYSI6ImNqNHVyaTc5dDBuazczMm1jenl3cG8wb3IifQ.2UEZ-jiHgHvYYqVirXhgpw\n',
@@ -79,8 +81,33 @@ onMounted(async () => {
   // const buildingTileset = await createOsmBuildingsAsync()
   // viewer.scene.primitives.add(buildingTileset)
 })
+const geoJsonData = {
+  type: "Feature",
+  geometry: {
+    type: "Point",
+    coordinates: [121.0, 24.0, 999999]
+  },
+  properties: {
+    name: "Null Island",
+    population: 4000,
+    rainfall: 0.3
+  }
+};
+const onload = result => {
+  console.log('load', result)
+
+  // viewer.dataSources.add(GeoJsonDataSource.load(geoJsonData))
+  //TODO： 图形会被遮盖的话，需要往坐标最后加个高程值，类似上面的 999999
+  viewer.dataSources.add(GeoJsonDataSource.load({
+    type: 'FeatureCollection',
+    features: result
+  }, {
+  }))
+}
 </script>
 
 <template>
+  <ShapeFile @load="onload"/>
+  <n-divider/>
   <div id="cesiumContainer" />
 </template>
